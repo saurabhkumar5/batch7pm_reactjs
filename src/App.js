fc89f29f78943a514c65d60173884182948ff7fe
saurabh kumar <saurabhkumar9493@gmail.com>
2024-08-31 19:46:28 +0530

    momo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import momo from './bmw.png';
-import home from './home.png';
-import video from "./video.mp4"
-import './App.css'
+// import momo from './bmw.png';
+// import home from './home.png';
+// import video from "./video.mp4"
+// import './App.css';
 
-      function App(){
+//       function App(){
  
           
-        return (
+//         return (
             
-           <div>
-             <h1 style={{backgroundColor:'yellow',color:'red',textAlign:'center'}}>helllo reactjs</h1>
-             <h1>kullu</h1>
-             <p>hello how are you</p>
-             <a href="https://saurabhkumar9493.netlify.app/">click here</a>
-             <img src={momo}/>
-             <img src={home}/>
-             <img src="https://img1.wsimg.com/isteam/ip/1172a89b-094a-415b-99fe-cc1e7d3cd9b0/blob-24cc2f2.png/:/cr=t:6.5%25,l:19.37%25,w:42.38%25,h:84.75%25/rs=w:365,h:487,cg:true,m/qt=q:51"/>
-             <img src='https://saurabhkumar9493.netlify.app/static/media/me-about.f4fedfbe5bd6bd897a07.png'/>
-             <video src={video} controls></video>
-            </div>
-        )
-      }
+//            <div>
+//              <h1 style={{backgroundColor:'yellow',color:'red',textAlign:'center'}}>helllo reactjs</h1>
+//              <h1 className='header'>kullu</h1>
+//              <p>hello how are you</p>
+//              <a href="https://saurabhkumar9493.netlify.app/">click here</a>
+//              <img src={momo}/>
+//              <img src={home}/>
+//              <img src="https://img1.wsimg.com/isteam/ip/1172a89b-094a-415b-99fe-cc1e7d3cd9b0/blob-24cc2f2.png/:/cr=t:6.5%25,l:19.37%25,w:42.38%25,h:84.75%25/rs=w:365,h:487,cg:true,m/qt=q:51"/>
+//              <img src='https://saurabhkumar9493.netlify.app/static/media/me-about.f4fedfbe5bd6bd897a07.png'/>
+//              <video src={video} controls></video>
+//             </div>
+//         )
+//       }
 
-      export default App;
+//       export default App;
+
+
+
+
+
+
+
+
+import './App.css'
+
+    function App(){
+
+        return(
+          <div>
+             <header>
+               <div className='logo'>Swiggy</div>
+               <ul className='nav'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>services</li>
+               </ul>
+             </header>
+
+
+             <div className="momo">
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597"/>
+                  <h1>Chinese Wok</h1>
+                  <h2>Rating:2.3 star</h2>
+                  <p>sector:10</p>
+             </div>
+
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/55d0daa1-ad2e-4893-be06-4709c5c68d49_41350.JPG"/>
+                  <h1>burger</h1>
+                  <h2>Rating:3 star</h2>
+                  <p>sector:25</p>
+             </div>
+
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/gnfaeaeddfxdzrxzoikp"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/auvtncksxjd2gg3b97dl"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bbuwmp35jmzxgpwyfq7b"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a6h2um4pfxtcxjdgerfb"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/auvtncksxjd2gg3b97dl"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bbuwmp35jmzxgpwyfq7b"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a6h2um4pfxtcxjdgerfb"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/auvtncksxjd2gg3b97dl"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/bbuwmp35jmzxgpwyfq7b"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             <div className='main'>
+                  <img className="img1" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/a6h2um4pfxtcxjdgerfb"/>
+                  <h1>dosa</h1>
+                  <h2>Rating:5 star</h2>
+                  <p>sector:25</p>
+             </div>
+             </div>
+
+             <iframe width="560" height="315" src="https://www.youtube.com/embed/es4x5R-rV9s?si=34hkUpJZFfqSrFWY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+ 
+             
+          </div>
+        )
+    }
+
+    export default App;
